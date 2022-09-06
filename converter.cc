01603ff09b58684d078e0042fa6b36d2637ca8c5
Hyxogen <8938732+Hyxogen@users.noreply.github.com>
2022-09-06 20:23:05 +0200

    format code

--- converter.cc
+++ converter.cc
@@ -1,79 +1,69 @@
 #include "converter.hh"
-#include <stdexcept>
-#include <span>
-#include <fstream>
-#include <assimp/scene.h>
 #include <assimp/postprocess.h>
+#include <assimp/scene.h>
 #include <assimp/texture.h>
+#include <fstream>
 #include <iostream>
+#include <span>
+#include <stdexcept>
 
-texture_converter::texture_converter(
-		const std::filesystem::path &from,
-		const std::filesystem::path &to) : _image(), from_path(from), to_path(to) {
-	if (!std::filesystem::exists(from))
-		throw std::runtime_error(from.string() + ": does not exist");
-	if (std::filesystem::status(from).type()
-				== std::filesystem::file_type::directory)
-		throw std::runtime_error(from.string() + ": is a directory");
-	_image.read(from_path.string());
+texture_converter::texture_converter(const std::filesystem::path &from,
+                                     const std::filesystem::path &to)
+    : _image(), from_path(from), to_path(to) {
+        if (!std::filesystem::exists(from))
+                throw std::runtime_error(from.string() + ": does not exist");
+        if (std::filesystem::status(from).type()
+            == std::filesystem::file_type::directory)
+                throw std::runtime_error(from.string() + ": is a directory");
+        _image.read(from_path.string());
 }
 
-texture_converter::texture_converter(
-		const std::string &from,
-		const std::string &to)
-	: texture_converter(std::filesystem::path(from),
-			std::filesystem::path(to)) { }
+texture_converter::texture_converter(const std::string &from,
+                                     const std::string &to)
+    : texture_converter(std::filesystem::path(from),
+                        std::filesystem::path(to)) {}
 
-texture_converter::~texture_converter() { }
+texture_converter::~texture_converter() {}
 
-void texture_converter::convert() {
-	_image.write(to_path.string());
-}
+void texture_converter::convert() { _image.write(to_path.string()); }
 
 converter::converter(const std::string &file, std::ostream &out)
-	: _file(file),
-	_out(out),
-	_importer(),
-	_scene(_importer.ReadFile(_file.c_str(), aiProcess_Triangulate)) { 
-}
+    : _file(file), _out(out), _importer(),
+      _scene(_importer.ReadFile(_file.c_str(), aiProcess_Triangulate)) {}
 
-void converter::convert() {
-	write_global_textures();
-}
+void converter::convert() { write_global_textures(); }
 
 void converter::write_global_textures() {
-	const std::span textures(_scene->mTextures, _scene->mNumTextures);
+        const std::span textures(_scene->mTextures, _scene->mNumTextures);
 
-	for (aiTexture *texture : textures) {
-		convert_texture(texture);
-	}
+        for (aiTexture *texture : textures) {
+                convert_texture(texture);
+        }
 }
 
 void converter::convert_texture(const aiTexture *texture) {
-	if (texture->mHeight == 0) {
-		convert_raw_texture(texture);
-	} else {
-		convert_compressed_texture(texture);
-	}
+        if (texture->mHeight == 0) {
+                convert_raw_texture(texture);
+        } else {
+                convert_compressed_texture(texture);
+        }
 }
 
 void converter::convert_raw_texture(const aiTexture *texture) {
-	(void) texture;
-	throw std::runtime_error("conversion of embedded textures is currently not implemented");
+        (void)texture;
+        throw std::runtime_error(
+            "conversion of embedded textures is currently not implemented");
 }
 
 std::string converter::texture_name(const std::string &path) {
-	return std::filesystem::path(path).stem().string();
+        return std::filesystem::path(path).stem().string();
 }
 
 void converter::convert_compressed_texture(const aiTexture *texture) {
-	const std::string name = texture_name(texture->mFilename.C_Str());
-	_out
-		<< TEX_DIRECTIVE
-		<< SEPARATOR
-		<< TEX_PREFIX
-		<< name << std::endl;
-	std::filesystem::path tex_path =
-		std::filesystem::current_path() / scene_name / (name + TEX_EXT);
-	texture_converter(texture->mFilename.C_Str(), tex_path.string()).convert();
+        const std::string name = texture_name(texture->mFilename.C_Str());
+        _out << TEX_DIRECTIVE << SEPARATOR << TEX_PREFIX << name << std::endl;
+        std::filesystem::path tex_path
+            = std::filesystem::current_path() / scene_name / (name + TEX_EXT);
+        texture_converter(texture->mFilename.C_Str(), tex_path.string())
+            .convert();
 }
--- converter.hh
+++ converter.hh
@@ -1,13 +1,13 @@
 #ifndef CONVERTER_HH
 #define CONVERTER_HH
 
-#include <string>
-#include <iostream>
-#include <unordered_map>
-#include <assimp/scene.h>
-#include <assimp/Importer.hpp>
-#include <filesystem>
 #include <Magick++.h>
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <filesystem>
+#include <iostream>
+#include <string>
+#include <unordered_map>
 
 const static std::string SEPARATOR = " ";
 const static std::string TEX_DIRECTIVE = "tex";
@@ -15,44 +15,44 @@ const static std::string TEX_PREFIX = "tex_";
 const static std::string TEX_EXT = ".bmp";
 
 class texture_converter {
-	Magick::Image _image;
-public:
-	const std::filesystem::path from_path, to_path;
+        Magick::Image _image;
 
-	texture_converter() = delete;
-	texture_converter(const std::filesystem::path &from,
-			const std::filesystem::path &to);
-	texture_converter(const std::string &from,
-			const std::string &to);
-	~texture_converter();
+      public:
+        const std::filesystem::path from_path, to_path;
 
-	void convert();
+        texture_converter() = delete;
+        texture_converter(const std::filesystem::path &from,
+                          const std::filesystem::path &to);
+        texture_converter(const std::string &from, const std::string &to);
+        ~texture_converter();
+
+        void convert();
 };
 
 class converter {
-	std::string _file;
-	std::ostream &_out;
-	Assimp::Importer _importer;
-	const aiScene *const _scene;
-	std::unordered_map<std::string, std::string> _textures;
+        std::string _file;
+        std::ostream &_out;
+        Assimp::Importer _importer;
+        const aiScene *const _scene;
+        std::unordered_map<std::string, std::string> _textures;
 
-public:
-	const std::string scene_name;
+      public:
+        const std::string scene_name;
 
-	converter() = delete;
-	converter(const std::string &file, std::ostream &out);
+        converter() = delete;
+        converter(const std::string &file, std::ostream &out);
 
-	void convert();
+        void convert();
 
-	inline const std::string &get_file() const { return _file; }
+        inline const std::string &get_file() const { return _file; }
 
-private:
-	void write_global_textures();
+      private:
+        void write_global_textures();
 
-	void convert_texture(const aiTexture *texture);
-	void convert_raw_texture(const aiTexture *texture);
-	void convert_compressed_texture(const aiTexture *texture);
-	static std::string texture_name(const std::string &path);
+        void convert_texture(const aiTexture *texture);
+        void convert_raw_texture(const aiTexture *texture);
+        void convert_compressed_texture(const aiTexture *texture);
+        static std::string texture_name(const std::string &path);
 };
 
 #endif
